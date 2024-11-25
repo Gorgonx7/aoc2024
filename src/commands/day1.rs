6f2e770309918cf405466b651d1b45687669d271
James Gordon <james.gordon2@ibm.com>
2024-11-25 23:23:16 +0000

    add simple tests

--- src/commands/day1.rs
+++ src/commands/day1.rs
@@ -10,9 +10,27 @@ pub struct Day1 {
 }
 
 impl Day1 {
+   
     pub fn execute(&self) -> Result<()> {
         let value = self.message.as_deref().unwrap_or("world");
         println!("hello {}", value);
         Ok(())
     }
+    pub fn good_add(&self, a: i16, b: i16) -> i16 {
+        a + b
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+// Note this useful idiom: importing names from outer (for mod tests) scope.
+ use super::*;
+
+ #[test]
+ fn test_add() {
+    let d = Day1{ message: None};
+     assert_eq!(d.good_add(1, 2), 3);
+ }
 }
